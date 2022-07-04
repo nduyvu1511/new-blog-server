b218e7612666a398a63aaa45d1e9dceb8c0eb5ec
vu <duyvujarvan4@gmail.com>
2022-07-04 10:35:43 +0700

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var morgan_1 = __importDefault(require("morgan"))
 var path_1 = __importDefault(require("path"))
 var config_1 = __importDefault(require("./config"))
 var routes_1 = __importDefault(require("./routes"))
-require("dotEnv").config()
+require("dotenv").config()
 var PORT = process.env.PORT || 5000
 var app = (0, express_1.default)()
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")))
